49ce6c55610f26764314d1c3af979a542cc42b82
Seongho Lee <tjdgh2626@gmail.com>
2022-11-30 17:55:36 +0900

    Implement interpret and execute function

--- msh.c
+++ msh.c
@@ -11,43 +11,10 @@
 #define FALSE   0
 #define TRUE    1
 
-#define EOL	1
-#define ARG	2
-#define AMPERSAND 3
-#define TOKEN_TYPE_REDIRECTION_R 4
-#define TOKEN_TYPE_REDIRECTION_L 5
-
-#define FOREGROUND 0
-#define BACKGROUND 1
-
 #define INPUT_SIZE  512
 
-static  char    input[INPUT_SIZE];
-static  char    tokens[1024];
-char            *ptr, *tok;
-
-int get_token(char** outptr) {
-    int	type;
-
-    *outptr = tok;
-    while ((*ptr == ' ') || (*ptr == '\t')) ptr++;
-
-    *tok++ = *ptr;
-
-    switch (*ptr++) {
-        case '\0' : type = EOL; break;
-        case '>': type = TOKEN_TYPE_REDIRECTION_R; break;
-        case '<': type = TOKEN_TYPE_REDIRECTION_L; break;
-        case '&': type = AMPERSAND; break;
-        default : type = ARG;
-            while ((*ptr != ' ') && (*ptr != '&') &&
-                (*ptr != '\t') && (*ptr != '\0'))
-                *tok++ = *ptr++;
-    }
-    *tok++ = '\0';
-
-    return type;
-}
+#define HOW_FOREGROUND  0
+#define HOW_BACKGROUND  1
 
 #define TOKEN_TYPE_NONE                 0
 #define TOKEN_TYPE_AMPERSAND            1
@@ -55,6 +22,7 @@ int get_token(char** outptr) {
 #define TOKEN_TYPE_RIGHT_ANGLE_BRACKET  3
 #define TOKEN_TYPE_DLEFT_ANGLE_BRACKET  4
 #define TOKEN_TYPE_DRIGHT_ANGLE_BRACKET 5
+#define TOKEN_TYPE_PIPE                 6
 
 typedef struct Tokenizer {
     char* token;
@@ -100,6 +68,7 @@ int _get_token_type(char* ch) {
         case '<': type = TOKEN_TYPE_LEFT_ANGLE_BRACKET; break;
         case '>': type = TOKEN_TYPE_RIGHT_ANGLE_BRACKET; break;
         case '&': type = TOKEN_TYPE_AMPERSAND; break;
+        case '|': type = TOKEN_TYPE_PIPE; break;
     }
 
     if (*(++ch) == '\0') {
@@ -164,106 +133,86 @@ int get_next_token(Tokenizer* tk) {
     return 1;
 }
 
-int execute(char **comm, int how, int redirection) {
+int is_token_last(Tokenizer* tk) {
+    return tk->_last;
+}
+
+int execute(char **argv, int how) {
     int	pid;
 
     if ((pid = fork()) < 0) {
-        fprintf(stderr, "minish : fork error\n");
-        return(-1);
-    }
-    else if (pid == 0) {
-        execvp(*comm, comm);
-        fprintf(stderr, "minish : command not found\n");
+        perror("fork error");
+        return -1;
+    } else if (pid == 0) {
+        /* child process */
+        execvp(*argv, argv);
+        fprintf(stderr, "msh: command not found\n");
         exit(127);
     }
-    if (how == BACKGROUND) {	/* Background execution */
-        printf("[%d]\n", pid);
+
+    /* parent process */
+    if (how == HOW_BACKGROUND) { // background execution
+        printf("background execution (PID: %d)\n", pid);
         return 0;
     }
-    /* Foreground execution */
-    while (waitpid(pid, NULL, 0) < 0)
+    while (waitpid(pid, NULL, 0) < 0) // foreground execution
         if (errno != EINTR) return -1;
+
     return 0;
 }
 
-int parse_and_execute(char* input)
-{
-    char* arg[1024];
-    int	type, how;
-    int	quit = FALSE;
-    int	narg = 0;
-    int	finished = FALSE;
-    int redirection = FALSE;
-    char redirection_arg[100];
-
-    ptr = input;
-    tok = tokens;
-    while (!finished) {
-        switch (type = get_token(&arg[narg])) {
-        case ARG:
-            narg++;
-            break;
-        case TOKEN_TYPE_REDIRECTION_R:
-            strcpy(redirection_arg, arg[narg]);
-            redirection = TOKEN_TYPE_REDIRECTION_R;
-            break;
-        case TOKEN_TYPE_REDIRECTION_L:
-            strcpy(redirection_arg, arg[narg]);
-            redirection = TOKEN_TYPE_REDIRECTION_L;
-            break;
-        case EOL:
-        case AMPERSAND:
-            if (!strcmp(arg[0], "quit")) quit = TRUE;
-            else if (!strcmp(arg[0], "exit")) quit = TRUE;
-            else if (!strcmp(arg[0], "cd")) {
-                if (chdir(arg[1]) == -1) {
-                    perror("chdir");
-                }
-            }
-            else if (!strcmp(arg[0], "type")) {
-                if (narg > 1) {
-                    int  fid;
-                    int	 readcount;
-                    char buf[512];
-                    if ((fid = open(arg[1], O_RDONLY)) >= 0) {
-                        while ((readcount = read(fid, &buf, 1)) > 0) {
-                            write(1, &buf, readcount);
-                        }
-                    } else {
-                        perror("open");
-                    }
-                    close(fid);
-                }
-            } else {
-                if (redirection) {
-                    printf("%s \n", redirection_arg);
-                }
-                how = (type == AMPERSAND) ? BACKGROUND : FOREGROUND;
-                arg[narg] = NULL;
-                if (narg != 0)
-                    execute(arg, how, 0);
-            }
-            narg = 0;
-            if (type == EOL)
-                finished = TRUE;
-            break; 
-        }
-
+int command(char** argv, int argc) {
+#define CMD(X) (!strcmp(*argv, (X)))
+    if (CMD("exit")) {
+        exit(0);
+    } else if (CMD("quit")) {
+        exit(0);
+    } else if (CMD("cd")) {
+        fprintf(stderr, "TODO: implement change directory. \n");
+    } else if (CMD("type")) {
+        fprintf(stderr, "TODO: implement text read command. \n");
+    } else {
+        return 1; // external command
     }
-    return quit;
+
+    return 0;
+#undef CMD
+}
+
+int interpret(char* input) {
+    Tokenizer tk;
+    init_tokenizer(&tk, input);
+    if (is_token_last(&tk)) return 0;
+
+    char* argv[1024];
+    int argc = 0;
+    int how = HOW_FOREGROUND;
+
+    while (get_next_token(&tk)) {
+        if (tk.type != TOKEN_TYPE_NONE) break;
+        argv[argc++] = tk.token;
+    }
+    argv[argc] = (char*)0;
+
+    switch (tk.type) {
+        case TOKEN_TYPE_AMPERSAND:
+            how = HOW_BACKGROUND; break;
+    }
+
+    if (command(argv, argc)) { // execute internal command
+        execute(argv, how); // execute external command
+    }
+
+    return 0;
 }
 
 int main(void)
 {
+    char input[INPUT_SIZE];
+
     printf("msh # ");
     while (fgets(input, INPUT_SIZE, stdin)) {
-        for (int i = 0; i < INPUT_SIZE; i++) {
-            if (input[i] == '\n') {
-                input[i] = '\0';
-                break;
-            }
-        }
-        if (parse_and_execute(input)) {
+        if (interpret(input)) {
             break;
         }
         printf("msh # ");
